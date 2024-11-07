a4dfbee6e88f00bc11c111b6434433859a8de77e
Daniel Samuel <dsamuel@student.42wolfsburg.de>
2024-11-07 19:49:23 +0100

    modify parser

--- src/parser/tokenizer.c
+++ src/parser/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 16:23:49 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/10/28 17:10:54 by dsamuel          ###   ########.fr       */
+/*   Updated: 2024/11/07 16:09:33 by dsamuel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,22 +26,22 @@
  */
 void ft_type_arg(t_cmd_token *token, int separator)
 {
-    if (ft_strcmp(token->content, "") == 0)
-        token->type = EMPTY;
-    else if (ft_strcmp(token->content, ">") == 0 && separator == 0)
-        token->type = TRUNC;
-    else if (ft_strcmp(token->content, ">>") == 0 && separator == 0)
-        token->type = APPEND;
-    else if (ft_strcmp(token->content, "<") == 0 && separator == 0)
-        token->type = INPUT;
-    else if (ft_strcmp(token->content, "|") == 0 && separator == 0)
-        token->type = PIPE;
-    else if (ft_strcmp(token->content, ";") == 0 && separator == 0)
-        token->type = END;
-    else if (token->prev == NULL || token->prev->type >= TRUNC)
-        token->type = CMD;
-    else
-        token->type = ARG;
+	if (ft_strcmp(token->content, "") == 0)
+		token->type = EMPTY;
+	else if (ft_strcmp(token->content, ">") == 0 && separator == 0)
+		token->type = TRUNC;
+	else if (ft_strcmp(token->content, ">>") == 0 && separator == 0)
+		token->type = APPEND;
+	else if (ft_strcmp(token->content, "<") == 0 && separator == 0)
+		token->type = INPUT;
+	else if (ft_strcmp(token->content, "|") == 0 && separator == 0)
+		token->type = PIPE;
+	else if (ft_strcmp(token->content, ";") == 0 && separator == 0)
+		token->type = END;
+	else if (token->prev == NULL || token->prev->type >= TRUNC)
+		token->type = CMD;
+	else
+		token->type = ARG;
 }
 
 /**
@@ -54,31 +54,43 @@ void ft_type_arg(t_cmd_token *token, int separator)
  * (TRUNC, APPEND, or INPUT). If so, it modifies the links in the token list, ensuring 
  * that the argument tokens are associated with their appropriate commands or separators.
  */
-void ft_squish_args(t_shell_state *shell_state)
+void	ft_squish_args(t_shell_state *shell_state)
 {
-    t_cmd_token *token;
-    t_cmd_token *prev;
+	t_cmd_token	*token;
+	t_cmd_token	*prev;
 
-    token = shell_state->cmd_list;
-    while (token)
-    {
-        prev = ft_prev_sep(token, NOSKIP);
-        if (ft_is_type(token, ARG) && ft_is_types(prev, "TAI"))
-        {
-            while (ft_is_last_valid_arg(prev) == 0)
-                prev = prev->prev;
-            token->prev->next = token->next;
-            if (token->next)
-                token->next->prev = token->prev;
-            token->prev = prev;
-            token->next = (prev) ? prev->next : shell_state->cmd_list;
-            prev = (prev) ? prev : token;
-            prev->next->prev = token;
-            prev->next = (shell_state->cmd_list->prev) ? prev->next : token;
-            shell_state->cmd_list = (shell_state->cmd_list->prev) ? shell_state->cmd_list->prev : shell_state->cmd_list;
-        }
-        token = token->next;
-    }
+	token = shell_state->cmd_list;
+	while (token)
+	{
+		prev = ft_prev_sep(token, NOSKIP);
+		if (ft_is_type(token, ARG) && ft_is_types(prev, "TAI"))
+		{
+			while (ft_is_last_valid_arg(prev) == 0)
+				prev = prev->prev;
+			token->prev->next = token->next;
+			if (token->next)
+				token->next->prev = token->prev;
+			token->prev = prev;
+			if (prev)
+				token->next = prev->next;
+			else
+				token->next = shell_state->cmd_list;
+			if (prev)
+				prev = prev;
+			else
+				prev = token;
+			prev->next->prev = token;
+			if (shell_state->cmd_list->prev)
+				prev->next = prev->next;
+			else
+				prev->next = token;
+			if (shell_state->cmd_list->prev)
+				shell_state->cmd_list = shell_state->cmd_list->prev;
+			else
+				shell_state->cmd_list = shell_state->cmd_list;
+		}
+		token = token->next;
+	}
 }
 
 /**
@@ -94,31 +106,31 @@ void ft_squish_args(t_shell_state *shell_state)
  * Return:
  * - The length of the next token, adjusted for special characters and quotes.
  */
-int ft_next_alloc(char *line, int *i)
+int	ft_next_alloc(char *line, int *i)
 {
-    int count;
-    int j;
-    char c;
+	int		count;
+	int		j;
+	char	c;
 
-    count = 0;
-    j = 0;
-    c = ' ';
-    while (line[*i + j] && (line[*i + j] != ' ' || c != ' '))
-    {
-        if (c == ' ' && (line[*i + j] == '\'' || line[*i + j] == '\"'))
-            c = line[*i + j++];
-        else if (c != ' ' && line[*i + j] == c)
-        {
-            count += 2;
-            c = ' ';
-            j++;
-        }
-        else
-            j++;
-        if (line[*i + j - 1] == '\\')
-            count--;
-    }
-    return (j - count + 1);
+	count = 0;
+	j = 0;
+	c = ' ';
+	while (line[*i + j] && (line[*i + j] != ' ' || c != ' '))
+	{
+		if (c == ' ' && (line[*i + j] == '\'' || line[*i + j] == '\"'))
+			c = line[*i + j++];
+		else if (c != ' ' && line[*i + j] == c)
+		{
+			count += 2;
+			c = ' ';
+			j++;
+		}
+		else
+			j++;
+		if (line[*i + j - 1] == '\\')
+			count--;
+	}
+	return (j - count + 1);
 }
 
 /**
@@ -136,32 +148,34 @@ int ft_next_alloc(char *line, int *i)
  * - A pointer to the newly created token.
  * - NULL if memory allocation fails.
  */
-t_cmd_token *ft_next_token(char *line, int *i)
+t_cmd_token	*ft_next_token(char *line, int *i)
 {
-    t_cmd_token *token;
-    int j;
-    char c;
+	t_cmd_token	*token;
+	int			j;
+	char		c;
 
-    j = 0;
-    c = ' ';
-    if (!(token = malloc(sizeof(t_cmd_token))) || !(token->content = malloc(sizeof(char) * ft_next_alloc(line, i))))
-        return (NULL);
-    while (line[*i] && (line[*i] != ' ' || c != ' '))
-    {
-        if (c == ' ' && (line[*i] == '\'' || line[*i] == '\"'))
-            c = line[(*i)++];
-        else if (c != ' ' && line[*i] == c)
-        {
-            c = ' ';
-            (*i)++;
-        }
-        else if (line[*i] == '\\' && (*i)++)
-            token->content[j++] = line[(*i)++];
-        else
-            token->content[j++] = line[(*i)++];
-    }
-    token->content[j] = '\0';
-    return (token);
+	j = 0;
+	c = ' ';
+	token = malloc(sizeof(t_cmd_token));
+	token->content = malloc(sizeof(char) * ft_next_alloc(line, i));
+	if (!token || !token->content)
+		return (NULL);
+	while (line[*i] && (line[*i] != ' ' || c != ' '))
+	{
+		if (c == ' ' && (line[*i] == '\'' || line[*i] == '\"'))
+			c = line[(*i)++];
+		else if (c != ' ' && line[*i] == c)
+		{
+			c = ' ';
+			(*i)++;
+		}
+		else if (line[*i] == '\\' && (*i)++)
+			token->content[j++] = line[(*i)++];
+		else
+			token->content[j++] = line[(*i)++];
+	}
+	token->content[j] = '\0';
+	return (token);
 }
 
 /**
@@ -177,31 +191,31 @@ t_cmd_token *ft_next_token(char *line, int *i)
  * Return:
  * - A pointer to the head of the linked list of tokens.
  */
-t_cmd_token *ft_get_tokens(char *line)
+t_cmd_token	*ft_get_tokens(char *line)
 {
-    t_cmd_token *prev;
-    t_cmd_token *next;
-    int i;
-    int sep;
+	t_cmd_token	*prev;
+	t_cmd_token	*next;
+	int			i;
+	int			sep;
 
-    prev = NULL;
-    next = NULL;
-    i = 0;
-    ft_skip_spacenl(line, &i);
-    while (line[i])
-    {
-        sep = ft_ignore_sep(line, i);
-        next = ft_next_token(line, &i);
-        next->prev = prev;
-        if (prev)
-            prev->next = next;
-        prev = next;
-        ft_type_arg(next, sep);
-        ft_skip_spacenl(line, &i);
-    }
-    if (next)
-        next->next = NULL;
-    while (next && next->prev)
-        next = next->prev;
-    return (next);
+	prev = NULL;
+	next = NULL;
+	i = 0;
+	ft_skip_spacenl(line, &i);
+	while (line[i])
+	{
+		sep = ft_ignore_sep(line, i);
+		next = ft_next_token(line, &i);
+		next->prev = prev;
+		if (prev)
+			prev->next = next;
+		prev = next;
+		ft_type_arg(next, sep);
+		ft_skip_spacenl(line, &i);
+	}
+	if (next)
+		next->next = NULL;
+	while (next && next->prev)
+		next = next->prev;
+	return (next);
 }
