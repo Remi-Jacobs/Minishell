93697851cd79b8cdf27a6b2594617c1e08ea9b4f
Daniel Samuel <dsamuel@student.42wolfsburg.de>
2024-11-17 19:31:20 +0100

    refactor env

--- src/env/env_vars.c
+++ src/env/env_vars.c
@@ -3,178 +3,140 @@
 /*                                                        :::      ::::::::   */
 /*   env_vars.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ojacobs <ojacobs@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:16:19 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/11/08 19:58:18 by ojacobs          ###   ########.fr       */
+/*   Updated: 2024/11/17 19:29:18 by dsamuel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/**
- * size_env - Calculates the total length of all environment variable values in a linked list.
- * 
- * @lst: A pointer to the first node in the linked list of environment variables (t_env).
- * 
- * This function iterates through the linked list `lst` and calculates the total length of all
- * non-NULL environment variable values. Each value's length is obtained using `ft_strlen()`, 
- * and an additional character is counted to account for the separator (newline or null).
- * 
- * Return:
- * - The total length of all environment variable values in the list.
- */
-size_t ft_size_env(t_env_variable *lst)
+size_t	ft_size_env(t_env_variable *lst)
 {
-    size_t lst_len;
+	size_t	lst_len;
 
-    lst_len = 0;
-    while (lst && lst->next != NULL)
-    {
-        if (lst->variable != NULL)
-        {
-            lst_len += ft_strlen(lst->variable);
-            lst_len++;
-        }
-        lst = lst->next;
-    }
-    return (lst_len);
+	lst_len = 0;
+	while (lst && lst->next != NULL)
+	{
+		if (lst->variable != NULL)
+		{
+			lst_len += ft_strlen(lst->variable);
+			lst_len++;
+		}
+		lst = lst->next;
+	}
+	return (lst_len);
 }
 
-/**
- * env_to_str - Converts a linked list of environment variables to a single string.
- * 
- * @lst: A pointer to the first node in the linked list of environment variables (t_env).
- * 
- * This function creates a single string `env` from the linked list of environment variables. 
- * It allocates memory for the string based on the total size calculated by `size_env()`. It 
- * then concatenates all non-NULL environment variable values, separating them with a newline.
- * 
- * Return:
- * - A pointer to the newly allocated string containing all environment variables separated by newlines.
- * - NULL if memory allocation fails.
- */
-char *ft_env_to_str(t_env_variable *lst)
+static void	ft_copy_env_variables(t_env_variable *lst, char *env, int *index)
 {
-    char *env;
-    int i;
-    int j;
+	int	j;
 
-    if (!(env = malloc(sizeof(char) * ft_size_env(lst) + 1)))
-        return (NULL);
-    i = 0;
-    while (lst && lst->next != NULL)
-    {
-        if (lst->variable != NULL)
-        {
-            j = 0;
-            while (lst->variable[j])
-            {
-                env[i] = lst->variable[j];
-                i++;
-                j++;
-            }
-        }
-        if (lst->next->next != NULL)
-            env[i++] = '\n';
-        lst = lst->next;
-    }
-    env[i] = '\0';
-    return (env);
+	while (lst && lst->next != NULL)
+	{
+		if (lst->variable != NULL)
+		{
+			j = 0;
+			while (lst->variable[j])
+			{
+				env[*index] = lst->variable[j];
+				(*index)++;
+				j++;
+			}
+		}
+		if (lst->next->next != NULL)
+			env[(*index)++] = '\n';
+		lst = lst->next;
+	}
 }
 
-/**
- * env_init - Initializes the environment linked list from an array of environment variables.
- * 
- * @shell_state: A pointer to the main shell state structure (t_mini) containing environment information.
- * @env_array: An array of strings representing environment variables.
- * 
- * This function initializes the `mini->env` linked list with values from the `env_array` array. 
- * It allocates memory for each node, duplicates the string, and connects each node to form a linked list.
- * 
- * Return:
- * - 0 if the initialization is successful.
- * - 1 if memory allocation fails.
- */
-int ft_env_init(t_shell_state *shell_state, char **env_array)
+char	*ft_env_to_str(t_env_variable *lst)
 {
-    t_env_variable *env;
-    t_env_variable *new;
-    int i;
+	char	*env;
+	int		i;
 
-    if (!(env = malloc(sizeof(t_env_variable))))
-        return (1);
-    env->variable = ft_strdup(env_array[0]);
-    env->next = NULL;
-    shell_state->active_env = env;
-    i = 1;
-    while (env_array && env_array[0] && env_array[i])
-    {
-        if (!(new = malloc(sizeof(t_env_variable))))
-            return (1);
-        new->variable = ft_strdup(env_array[i]);
-        new->next = NULL;
-        env->next = new;
-        env = new;
-        i++;
-    }
-    return (0);
+	i = 0;
+	env = malloc(sizeof(char) * ft_size_env(lst) + 1);
+	if (!env)
+		return (NULL);
+	ft_copy_env_variables(lst, env, &i);
+	env[i] = '\0';
+	return (env);
 }
 
+// char	*ft_env_to_str(t_env_variable *lst)
+// {
+// 	char	*env;
+// 	int		i;
+// 	int		j;
 
-static void  ft_get_home_directory(t_shell_state *shell_state, char **envp)
+// 	env = malloc(sizeof(char) * ft_size_env(lst) + 1);
+// 	if (!env)
+// 		return (NULL);
+// 	i = 0;
+// 	while (lst && lst->next != NULL)
+// 	{
+// 		if (lst->variable != NULL)
+// 		{
+// 			j = 0;
+// 			while (lst->variable[j])
+// 			{
+// 				env[i] = lst->variable[j];
+// 				i++;
+// 				j++;
+// 			}
+// 		}
+// 		if (lst->next->next != NULL)
+// 			env[i++] = '\n';
+// 		lst = lst->next;
+// 	}
+// 	env[i] = '\0';
+// 	return (env);
+// }
+
+static void	ft_get_home_directory(t_shell_state *shell_state, char **envp)
 {
-    	int	i;
+	int	i;
 
 	i = 0;
 	while (envp[i] && ft_strncmp(envp[i], "HOME=", 5) != 0)
 		i++;
 	if (envp[i] && ft_strncmp(envp[i], "HOME=", 5) == 0)
 	{
-        shell_state->tilde = ft_strdup (&envp[i][5]);
+		shell_state->tilde = ft_strdup (&envp[i][5]);
 		if (!shell_state->tilde)
 		{
 			perror("erro copying home");
 			exit(EXIT_FAILURE);
 		}
-    }
+	}
 }
 
-/**
- * secret_env_init - Initializes the secret environment linked list from an array of environment variables.
- * 
- * @shell_array: A pointer to the main shell state structure (t_mini) containing secret environment information.
- * @env_array: An array of strings representing environment variables.
- * 
- * This function initializes the `mini->secret_env` linked list with values from the `env_array` array.
- * It allocates memory for each node, duplicates the string, and connects each node to form a linked list.
- * 
- * Return:
- * - 0 if the initialization is successful.
- * - 1 if memory allocation fails.
- */
-int ft_secret_env_init(t_shell_state *shell_state, char **env_array)
+int	ft_secret_env_init(t_shell_state *shell_state, char **env_array)
 {
-    t_env_variable *env;
-    t_env_variable *new;
-    int i;
+	t_env_variable	*env;
+	t_env_variable	*new;
+	int				i;
 
-    if (!(env = malloc(sizeof(t_env_variable))))
-        return (1);
-    env->variable = ft_strdup(env_array[0]);
-    env->next = NULL;
-    shell_state->secret_env = env;
-    i = 1;
-    while (env_array && env_array[0] && env_array[i])
-    {
-        if (!(new = malloc(sizeof(t_env_variable))))
-            return (1);
-        new->variable = ft_strdup(env_array[i]);
-        new->next = NULL;
-        env->next = new;
-        env = new;
-        i++;
-    }
-    ft_get_home_directory(shell_state, env_array);
-    return (0);
+	env = malloc(sizeof(t_env_variable));
+	if (!env)
+		return (1);
+	env->variable = ft_strdup(env_array[0]);
+	env->next = NULL;
+	shell_state->secret_env = env;
+	i = 1;
+	while (env_array && env_array[0] && env_array[i])
+	{
+		new = malloc(sizeof(t_env_variable));
+		if (!new)
+			return (1);
+		new->variable = ft_strdup(env_array[i]);
+		new->next = NULL;
+		env->next = new;
+		env = new;
+		i++;
+	}
+	ft_get_home_directory(shell_state, env_array);
+	return (0);
 }
--- src/env/sort_env.c
+++ src/env/sort_env.c
@@ -6,107 +6,91 @@
 /*   By: dsamuel <dsamuel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 15:15:58 by dsamuel           #+#    #+#             */
-/*   Updated: 2024/10/22 19:35:47 by dsamuel          ###   ########.fr       */
+/*   Updated: 2024/11/17 19:07:16 by dsamuel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/**
- * str_env_len - Calculates the number of strings in an array of environment variables.
- * 
- * @env: A double pointer to an array of strings representing the environment variables.
- * 
- * This function iterates through the array of strings `env` and counts the number of elements present.
- * It stops counting when it encounters a NULL pointer, which indicates the end of the array.
- * 
- * Return:
- * - The number of strings (environment variables) in the array.
- */
-int ft_str_env_len(char **env)
+int	ft_str_env_len(char **env)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (env[i])
-        i++;
-    return (i);
+	i = 0;
+	while (env[i])
+		i++;
+	return (i);
 }
 
-/**
- * sort_env - Sorts an array of environment variable strings in alphabetical order.
- * 
- * @tab: A double pointer to an array of strings representing the environment variables to be sorted.
- * @env_len: The length of the array of strings.
- * 
- * This function sorts the array `tab` in place using a bubble sort algorithm. It compares each pair 
- * of adjacent strings and swaps them if they are out of order, continuing the process until the entire 
- * array is sorted in ascending alphabetical order. The sorting is based on the result of `ft_strcmp()`.
- * 
- * Return:
- * - This function does not return a value.
- */
-void ft_sort_env(char **tab, int env_len)
+void	ft_sort_env(char **tab, int env_len)
 {
-    int ordered;
-    int i;
-    char *tmp;
+	int		ordered;
+	int		i;
+	char	*tmp;
 
-    ordered = 0;
-    while (tab && ordered == 0)
-    {
-        ordered = 1;
-        i = 0;
-        while (i < env_len - 1)
-        {
-            if (ft_strcmp(tab[i], tab[i + 1]) > 0)
-            {
-                tmp = tab[i];
-                tab[i] = tab[i + 1];
-                tab[i + 1] = tmp;
-                ordered = 0;
-            }
-            i++;
-        }
-        env_len--;
-    }
+	ordered = 0;
+	while (tab && ordered == 0)
+	{
+		ordered = 1;
+		i = 0;
+		while (i < env_len - 1)
+		{
+			if (ft_strcmp(tab[i], tab[i + 1]) > 0)
+			{
+				tmp = tab[i];
+				tab[i] = tab[i + 1];
+				tab[i + 1] = tmp;
+				ordered = 0;
+			}
+			i++;
+		}
+		env_len--;
+	}
 }
 
-/**
- * print_sorted_env - Prints the sorted list of environment variables in the format "declare -x".
- * 
- * @env: A pointer to a linked list of environment variables (t_env).
- * 
- * This function converts the linked list of environment variables `env` into a single string, which is 
- * then split into an array of strings. It sorts this array of strings alphabetically using `sort_env()`. 
- * Each sorted string is printed to the standard output in the format `"declare -x <variable>"`.
- * 
- * How it works:
- * - Converts the linked list `env` into a single string using `env_to_str()`.
- * - Splits the resulting string into an array of strings, where each element represents an environment variable.
- * - Sorts the array of environment variables alphabetically using `sort_env()`.
- * - Prints each sorted environment variable with the prefix `"declare -x "` using `ft_putstr()` and `ft_putendl()`.
- * - Frees the allocated memory for the array of strings using `free_tab()`.
- * 
- * Return:
- * - This function does not return a value.
- */
-void ft_print_sorted_env(t_env_variable *env)
+int	ft_env_init(t_shell_state *shell_state, char **env_array)
 {
-    int i;
-    char **tab;
-    char *str_env;
+	t_env_variable	*env;
+	t_env_variable	*new;
+	int				i;
 
-    str_env = ft_env_to_str(env);
-    tab = ft_split(str_env, '\n');
-    ft_memdel(str_env);
-    ft_sort_env(tab, ft_str_env_len(tab));
-    i = 0;
-    while (tab[i])
-    {
-        ft_putstr("declare -x ");
-        ft_putendl(tab[i]);
-        i++;
-    }
-    ft_free_tab(tab);
+	env = malloc(sizeof(t_env_variable));
+	if (!env)
+		return (1);
+	env->variable = ft_strdup(env_array[0]);
+	env->next = NULL;
+	shell_state->active_env = env;
+	i = 1;
+	while (env_array && env_array[0] && env_array[i])
+	{
+		new = malloc(sizeof(t_env_variable));
+		if (!new)
+			return (1);
+		new->variable = ft_strdup(env_array[i]);
+		new->next = NULL;
+		env->next = new;
+		env = new;
+		i++;
+	}
+	return (0);
+}
+
+void	ft_print_sorted_env(t_env_variable *env)
+{
+	int		i;
+	char	**tab;
+	char	*str_env;
+
+	str_env = ft_env_to_str(env);
+	tab = ft_split(str_env, '\n');
+	ft_memdel(str_env);
+	ft_sort_env(tab, ft_str_env_len(tab));
+	i = 0;
+	while (tab[i])
+	{
+		ft_putstr("declare -x ");
+		ft_putendl(tab[i]);
+		i++;
+	}
+	ft_free_tab(tab);
 }
